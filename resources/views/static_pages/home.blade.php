5576f7ee4830fce39493116fd4783b68b432fdb1
vagrant <vagrant@homestead>
2019-12-19 06:43:22 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
-@section('title', '帮助')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training/6.x">Web 开发实战入门</a>
+ </p>
+ <p>
+ 一切，将从这里开始。
+ </p>
+ <p>
+ <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
